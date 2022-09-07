18e9eab97b585d5fc0f986815a965524a79926d2
sarahalomar <1saronho@gmail.com>
2022-09-07 20:24:16 +0300

    fix conflict

--- herfaty/lib/main.dart
+++ herfaty/lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:herfaty/pages/login.dart';
 import 'package:herfaty/pages/welcome.dart';
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
  
