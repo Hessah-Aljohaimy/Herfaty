7950e7dc20dfc0cb4c503829b54fab9406b0f18e
hessahju <hheessaahh2001@gmail.com>
2022-09-08 20:06:10 +0300

    restart

--- herfaty/lib/constants/colors.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class myColors {
-  static const Color darkGreen = Color(0x51908E);
-  static const Color lightBlue = Color(0x9BD6E6);
-  static const Color lightOrange = Color(0xF8C695);
-  static const Color Orange = Color(0xF8C695);
-  static const Color pink = Color(0xF38694);
-}
