d24d3a88ea3cf28b69b7985b44fba194eea6ad23
Razan Hamid <99924345+Raazanh@users.noreply.github.com>
2022-09-08 13:07:50 +0300

    first

---
+++ herfaty/lib/ShopOwnerProduct.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ herfaty/lib/category.dart
@@ -0,0 +1,19 @@
+
+
+class category {
+  string icon;
+  String 
+  String imgName;
+  //List <category> subCatetegories;
+
+
+Center(
+  child: Container(
+    margin: const EdgeInsets.all(10.0),
+    color: Colors.amber[600],
+    width: 48.0,
+    height: 48.0,
+  )
+)
+
+}
