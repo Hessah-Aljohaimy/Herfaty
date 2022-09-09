2e19329b44481f91293f592df080bfbb58412a7a
hessahju <hheessaahh2001@gmail.com>
2022-09-09 07:37:33 +0300

    taking the reusable widgets from login

---
+++ herfaty/lib/pages/reusable_widgits.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+TextFormField reusableTextField(String text, IconData icon, bool isPasswordType,
+    TextEditingController controller) {
+  return TextFormField(
+    controller: controller,
+    obscureText: isPasswordType,
+    enableSuggestions: !isPasswordType,
+    autocorrect: !isPasswordType,
+    style: TextStyle(color: Color.fromARGB(255, 26, 96, 91)),
+    decoration: InputDecoration(
+      suffix: Icon(
+        Icons.person,
+        color: Color.fromARGB(255, 26, 96, 91),
+      ),
+      labelText: text,
+      labelStyle: TextStyle(color: Color.fromARGB(255, 26, 96, 91)),
+      // floatingLabelBehavior: FloatingLabelBehavior.never,
+      fillColor: Colors.white.withOpacity(0.3),
+
+      enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: Color.fromARGB(255, 26, 96, 91)),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderSide:
+            BorderSide(width: 2, color: Color.fromARGB(255, 26, 96, 91)),
+      ),
+    ),
+    keyboardType: isPasswordType
+        ? TextInputType.visiblePassword
+        : TextInputType.emailAddress,
+  );
+}
+
+Container firebaseUIButton(BuildContext context, String title, Function onTap) {
+  return Container(
+    width: MediaQuery.of(context).size.width,
+    height: 50,
+    margin: const EdgeInsets.fromLTRB(0, 10, 0, 20),
+    decoration: BoxDecoration(borderRadius: BorderRadius.circular(90)),
+    child: ElevatedButton(
+      onPressed: () {
+        onTap();
+      },
+      child: Text(
+        title,
+        style: const TextStyle(
+            color: Colors.black87, fontWeight: FontWeight.bold, fontSize: 16),
+      ),
+      style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.resolveWith((states) {
+            if (states.contains(MaterialState.pressed)) {
+              return Colors.black26;
+            }
+            return Colors.white;
+          }),
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)))),
+    ),
+  );
+}
