585943885be0193a16c41c642d7d3000aaa1b4eb
sarahalomar <1saronho@gmail.com>
2022-09-12 21:33:18 +0300

    remove

--- herfaty/lib/pages/signupCustomer.dart
+++ herfaty/lib/pages/signupCustomer.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:herfaty/pages/home_screen.dart';
-
+//import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:herfaty/pages/reusable_widgets.dart';
 import 'package:herfaty/pages/login.dart';
 
