dbf4f5fc3709b732fa94c602e34497da7ab2dfa0
hessahju <hheessaahh2001@gmail.com>
2022-09-20 17:13:40 +0300

    adding dart classes

---
+++ herfaty/lib/profile screens/CustomerProfile.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class CustomerProfile extends StatefulWidget {
+  const CustomerProfile({super.key});
+
+  @override
+  State<CustomerProfile> createState() => _CustomerProfileState();
+}
+
+class _CustomerProfileState extends State<CustomerProfile> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ herfaty/lib/profile screens/ShopOwnerProfile.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class ShopOwnerProfile extends StatefulWidget {
+  const ShopOwnerProfile({super.key});
+
+  @override
+  State<ShopOwnerProfile> createState() => _ShopOwnerProfileState();
+}
+
+class _ShopOwnerProfileState extends State<ShopOwnerProfile> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
