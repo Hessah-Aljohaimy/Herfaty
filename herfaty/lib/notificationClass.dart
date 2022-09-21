42faba5af129058821c68e49b33fb3bd83459a2f
Sarrahx <112611661+Sarrahx@users.noreply.github.com>
2022-09-21 13:15:36 +0300

    listen to DB
    get notification when a new document added to the collection "orders".
    There is a small issue that it shows a notification always when the app first run...

--- herfaty/lib/notificationClass.dart
+++ herfaty/lib/notificationClass.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:herfaty/LocalNotificationService.dart';
 import 'package:herfaty/secondNotificationScreen.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class notificationClass extends StatefulWidget {
   const notificationClass({super.key});
@@ -16,11 +18,13 @@ class _notificationClassState extends State<notificationClass> {
     service = LocalNotificationService();
     service.intialize();
     listenToNotification();
+
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
+    listenToDB();
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flutter Notification Demo'),
@@ -40,26 +44,26 @@ class _notificationClassState extends State<notificationClass> {
                     'This is a demo of how to use local notifications in Flutter.',
                     style: TextStyle(fontSize: 20),
                   ),
-                  ElevatedButton(
-                    onPressed: () async {
-                      await service.showNotification(
-                          id: 0,
-                          title: 'Notification Title',
-                          body: 'Some body simple');
-                    },
-                    child: const Text('Show Local Notification'),
-                  ),
-                  ElevatedButton(
-                    onPressed: () async {
-                      await service.showScheduledNotification(
-                        id: 0,
-                        title: 'Notification Title',
-                        body: 'Some body scheduled',
-                        seconds: 4,
-                      );
-                    },
-                    child: const Text('Show Scheduled Notification'),
-                  ),
+                  // ElevatedButton(
+                  //   onPressed: () async {
+                  //     await service.showNotification(
+                  //         id: 0,
+                  //         title: 'Notification Title',
+                  //         body: 'Some body simple');
+                  //   },
+                  //   child: const Text('Show Local Notification'),
+                  // ),
+                  // ElevatedButton(
+                  //   onPressed: () async {
+                  //     await service.showScheduledNotification(
+                  //       id: 0,
+                  //       title: 'Notification Title',
+                  //       body: 'Some body scheduled',
+                  //       seconds: 4,
+                  //     );
+                  //   },
+                  //   child: const Text('Show Scheduled Notification'),
+                  // ),
                   ElevatedButton(
                     onPressed: () async {
                       await service.showNotificationWithPayload(
@@ -79,9 +83,30 @@ class _notificationClassState extends State<notificationClass> {
     );
   }
 
+  void listenToDB() {
+    print("the notification form firestore method was called");
+    CollectionReference reference =
+        FirebaseFirestore.instance.collection('orders');
+    reference.snapshots().listen((querySnapshot) {
+      querySnapshot.docChanges.forEach((change) {
+        createNotification(
+            0, "a new order", "you have a new order, tap to view", "");
+      });
+    });
+  }
+
+  void createNotification(
+      int id, String title, String body, String payload) async {
+    await service.showNotificationWithPayload(
+        id: id,
+        title: title,
+        body: body,
+        //what should be the payload?
+        payload: 'payload content');
+  }
+
   void listenToNotification() =>
       service.onNotificationClick.stream.listen(onNoticationListener);
-
   void onNoticationListener(String? payload) {
     if (payload != null && payload.isNotEmpty) {
       print('payload: $payload');
