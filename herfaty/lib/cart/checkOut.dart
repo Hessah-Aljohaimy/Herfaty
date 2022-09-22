b052a8d1f7198ca9a655949fa33b72f429928e6a
aishaAboalatta <auoosh2000@gmail.com>
2022-09-22 17:07:37 +0300

    some editings

--- herfaty/lib/cart/checkOut.dart
+++ herfaty/lib/cart/checkOut.dart
@@ -1,30 +1,46 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
+import 'package:herfaty/constants/color.dart';
+import 'package:status_change/status_change.dart';
 
-class checkOut extends StatelessWidget {
+class checkOut extends StatefulWidget {
   const checkOut({Key? key}) : super(key: key);
 
   @override
+  _checkOutState createState() => _checkOutState();
+}
+
+class _checkOutState extends State<checkOut> {
+  @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        Text(
-          ' Card Form ',
-          style: Theme.of(context).textTheme.headline5,
-        ), // Text
-        const SizedBox(height: 20),
-        CardFormField(
-          controller: CardFormEditController(),
-        ), // CardFormField
-        const SizedBox(height: 10),
-        ElevatedButton(
-          onPressed: () {},
-          child: const Text(' Pay'),
-        ), // ElevatedButton
-      ],
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: SystemUiOverlayStyle.light,
+      child: Scaffold(
+        appBar: DefaultAppBar(title: "إكمال عملية الدفع"),
+      ),
     ); // Column
+  }
+}
+
+class DefaultAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+  const DefaultAppBar({
+    Key? key,
+    required this.title,
+  }) : super(key: key);
+
+  @override
+  Size get preferredSize => Size.fromHeight(56.0);
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Text(title, style: TextStyle(color: kPrimaryColor)),
+      centerTitle: true,
+      backgroundColor: Colors.white,
+      elevation: 0,
+      automaticallyImplyLeading: false,
+      iconTheme: IconThemeData(color: kPrimaryColor),
+    );
   }
 }
