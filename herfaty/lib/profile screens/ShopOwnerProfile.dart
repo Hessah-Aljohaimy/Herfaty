d1b5a88140f250a31a234e1e9abb5110f7613884
hessahju <hheessaahh2001@gmail.com>
2022-09-23 17:27:15 +0300

    edits

--- herfaty/lib/profile screens/ShopOwnerProfile.dart
+++ herfaty/lib/profile screens/ShopOwnerProfile.dart
@@ -9,6 +9,7 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:herfaty/AddProduct.dart';
 import 'package:herfaty/constants/color.dart';
+import 'package:herfaty/pages/login.dart';
 import 'package:herfaty/pages/signupHerafy.dart';
 import 'package:image_picker/image_picker.dart';
 
@@ -20,6 +21,15 @@ class ShopOwnerProfile extends StatefulWidget {
 }
 
 class _ShopOwnerProfileState extends State<ShopOwnerProfile> {
+  final FirebaseAuth auth = FirebaseAuth.instance;
+  User? user;
+  var uid;
+
+  _ShopOwnerProfileState() {
+    user = auth.currentUser;
+    uid = user!.uid;
+  }
+
   final CollectionReference shopOwners =
       FirebaseFirestore.instance.collection('shop_owner');
   late ShopOwner shopOwner;
@@ -68,6 +78,13 @@ class _ShopOwnerProfileState extends State<ShopOwnerProfile> {
           centerTitle: true,
           backgroundColor: Colors.white,
           elevation: 0,
+          leading: IconButton(
+            icon: Icon(Icons.logout, color: kPrimaryColor),
+            onPressed: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                  builder: (BuildContext context) => login()));
+            },
+          ),
           automaticallyImplyLeading: false,
           iconTheme: IconThemeData(color: kPrimaryColor),
         ),
@@ -118,7 +135,7 @@ class _ShopOwnerProfileState extends State<ShopOwnerProfile> {
     return Center(
       child: Stack(children: <Widget>[
         CircleAvatar(
-          radius: 80.0,
+          radius: 60.0,
           backgroundImage: _imageFile == null
               ? AssetImage("assets/images/Circular_Logo.png") as ImageProvider
               : FileImage(File(_imageFile!.path)),
@@ -128,23 +145,23 @@ class _ShopOwnerProfileState extends State<ShopOwnerProfile> {
           // ?  as ImageProvider
           // :
         ),
-        Positioned(
-          bottom: 20.0,
-          right: 20.0,
-          child: InkWell(
-            onTap: () {
-              showModalBottomSheet(
-                context: context,
-                builder: ((builder) => bottomSheet()),
-              );
-            },
-            child: Icon(
-              Icons.camera_alt,
-              color: Colors.teal,
-              size: 28.0,
-            ),
-          ),
-        ),
+        // Positioned(
+        //   bottom: 15.0,
+        //   right: 10.0,
+        //   child: InkWell(
+        //     onTap: () {
+        //       showModalBottomSheet(
+        //         context: context,
+        //         builder: ((builder) => bottomSheet()),
+        //       );
+        //     },
+        //     child: Icon(
+        //       Icons.camera_alt,
+        //       color: Colors.teal,
+        //       size: 22.0,
+        //     ),
+        //   ),
+        // ),
       ]),
     );
   }
@@ -208,11 +225,241 @@ class _ShopOwnerProfileState extends State<ShopOwnerProfile> {
     );
   }
 
-  Widget buildOwner(ShopOwner so) => Container(child: Text(so.email));
+//////////////////////////////////Building User Profile////////////////////////////////////////
+  Widget buildOwner(ShopOwner so) {
+    logo = so.logo;
+    return Container(
+      // padding: EdgeInsets.only(left: 16, top: 25, right: 16),
+      child: GestureDetector(
+          onTap: () {
+            FocusScope.of(context).unfocus();
+          },
+          child: ListView(
+            children: [
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(20),
+                            bottomRight: Radius.circular(20)),
+                        image: DecorationImage(
+                          image: AssetImage("assets/images/BG2.png"),
+                          fit: BoxFit.cover,
+                        )),
+                    child: Center(
+                      child: Column(
+                        children: [
+                          // Text(
+                          //   "معلومات حسابي",
+                          //   style: TextStyle(
+                          //       fontSize: 25,
+                          //       fontWeight: FontWeight.w800,
+                          //       color: Colors.black),
+                          // ),
+                          imageProfile(),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                margin: const EdgeInsets.all(10.0),
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 255, 251, 245),
+                  borderRadius: BorderRadius.all(Radius.circular(20)),
+                ),
+                child: SizedBox(
+                  height: 320,
+                  child: Column(
+                    children: [
+                      Expanded(
+                          child: Row(
+                        children: <Widget>[
+                          Text(
+                            "الاسم  ",
+                            style: TextStyle(
+                                fontSize: 17,
+                                fontWeight: FontWeight.w700,
+                                color: Color.fromARGB(255, 26, 96, 91)),
+                          ),
+                          Text(
+                            "  ${so.name}",
+                            style: TextStyle(
+                                fontSize: 17,
+                                fontWeight: FontWeight.w400,
+                                color: Colors.black),
+                          )
+                        ],
+                      )),
+                      Expanded(
+                          child: Row(
+                        children: <Widget>[
+                          Text(
+                            "البريد الالكتروني",
+                            style: TextStyle(
+                                fontSize: 17,
+                                fontWeight: FontWeight.w700,
+                                color: Color.fromARGB(255, 26, 96, 91)),
+                          ),
+                          Text(
+                            "  ${so.email}",
+                            style: TextStyle(
+                                fontSize: 17,
+                                fontWeight: FontWeight.w400,
+                                color: Colors.black),
+                          )
+                        ],
+                      )),
+                      Expanded(
+                          child: Row(
+                        children: <Widget>[
+                          Text(
+                            "تاريخ الميلاد",
+                            style: TextStyle(
+                                fontSize: 17,
+                                fontWeight: FontWeight.w700,
+                                color: Color.fromARGB(255, 26, 96, 91)),
+                          ),
+                          Text(
+                            "  ${so.DOB}",
+                            style: TextStyle(
+                                fontSize: 17,
+                                fontWeight: FontWeight.w400,
+                                color: Colors.black),
+                          )
+                        ],
+                      )),
+                      Expanded(
+                          child: Row(
+                        children: <Widget>[
+                          Text(
+                            "رقم الجوال",
+                            style: TextStyle(
+                                fontSize: 17,
+                                fontWeight: FontWeight.w700,
+                                color: Color.fromARGB(255, 26, 96, 91)),
+                          ),
+                          Text(
+                            "  ${so.phone_number}",
+                            style: TextStyle(
+                                fontSize: 17,
+                                fontWeight: FontWeight.w400,
+                                color: Colors.black),
+                          )
+                        ],
+                      )),
+                      Expanded(
+                          child: Row(
+                        children: <Widget>[
+                          Text(
+                            "اسم المتجر",
+                            style: TextStyle(
+                                fontSize: 17,
+                                fontWeight: FontWeight.w700,
+                                color: Color.fromARGB(255, 26, 96, 91)),
+                          ),
+                          Text(
+                            "  ${so.shopname}",
+                            style: TextStyle(
+                                fontSize: 17,
+                                fontWeight: FontWeight.w400,
+                                color: Colors.black),
+                          )
+                        ],
+                      )),
+                      Expanded(
+                          child: Row(
+                        children: <Widget>[
+                          Text(
+                            "وصف المتجر",
+                            style: TextStyle(
+                                fontSize: 17,
+                                fontWeight: FontWeight.w700,
+                                color: Color.fromARGB(255, 26, 96, 91)),
+                          ),
+                          Text(
+                            "  ${so.shopdescription}",
+                            style: TextStyle(
+                                fontSize: 17,
+                                fontWeight: FontWeight.w400,
+                                color: Colors.black),
+                          )
+                        ],
+                      )),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Row(children: [
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all(Color(0xff51908E)),
+                      padding: MaterialStateProperty.all(
+                          EdgeInsets.symmetric(horizontal: 50, vertical: 13)),
+                      shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(27))),
+                    ),
+                    child: Text(
+                      " تعديل البيانات",
+                      style: TextStyle(
+                          fontSize: 14,
+                          fontFamily: "Tajawal",
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all(Color(0xff51908E)),
+                      padding: MaterialStateProperty.all(
+                          EdgeInsets.symmetric(horizontal: 50, vertical: 13)),
+                      shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(27))),
+                    ),
+                    child: Text(
+                      "حذف الحساب",
+                      style: TextStyle(
+                          fontSize: 14,
+                          fontFamily: "Tajawal",
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ]),
+              )
+              // TextField(
+              //   decoration: InputDecoration(
+              //       labelText: 'الاسم',
+              //       floatingLabelBehavior: FloatingLabelBehavior.always,
+              //       hintText: so.name,
+              //       hintStyle: TextStyle(
+              //           fontSize: 20,
+              //           fontWeight: FontWeight.bold,
+              //           color: Color.fromARGB(148, 29, 29, 29))),
+              // ),
+              // Text(so.email)
+            ],
+          )),
+    );
+  }
+
 //Read a single shop owner
   Future<ShopOwner?> readShopOwner() async {
     final docShopOwner =
-        FirebaseFirestore.instance.collection("shop_owner").doc(id);
+        FirebaseFirestore.instance.collection("shop_owner").doc(uid);
   }
 
 //logo
@@ -253,6 +500,10 @@ class _ShopOwnerProfileState extends State<ShopOwnerProfile> {
     });
   }
 
+  // Future<String> getCurrentUID() async {
+  //   return (await _firebaseAuth.currentUser!).uid;
+  // }
+
   Future<ShopOwner?> readUser() async {
     //get a singal document
     final docShopOwner = FirebaseFirestore.instance
@@ -277,25 +528,25 @@ class _ShopOwnerProfileState extends State<ShopOwnerProfile> {
   //   print("uploaded:" + uploadImageUrl);
   // }
 }
-  // body: Container(
-      //   child: Column(
-      //     crossAxisAlignment: CrossAxisAlignment.center,
-      //     mainAxisAlignment: MainAxisAlignment.center,
-      //     children: [
-      //       imageProfile(),
-      //       const SizedBox(
-      //         height: 5,
-      //       ),
-      //       Row(
-      //         crossAxisAlignment: CrossAxisAlignment.center,
-      //         mainAxisAlignment: MainAxisAlignment.center,
-      //         children: [
-      //           Text(
-      //             ' الاسم' + DOB!,
-      //           ),
-      //           IconButton(onPressed: () {}, icon: const Icon(Icons.edit))
-      //         ],
-      //       )
-      //     ],
-      //   ),
-      // ),
+// body: Container(
+//   child: Column(
+//     crossAxisAlignment: CrossAxisAlignment.center,
+//     mainAxisAlignment: MainAxisAlignment.center,
+//     children: [
+//       imageProfile(),
+//       const SizedBox(
+//         height: 5,
+//       ),
+//       Row(
+//         crossAxisAlignment: CrossAxisAlignment.center,
+//         mainAxisAlignment: MainAxisAlignment.center,
+//         children: [
+//           Text(
+//             ' الاسم' + DOB!,
+//           ),
+//           IconButton(onPressed: () {}, icon: const Icon(Icons.edit))
+//         ],
+//       )
+//     ],
+//   ),
+// ),
