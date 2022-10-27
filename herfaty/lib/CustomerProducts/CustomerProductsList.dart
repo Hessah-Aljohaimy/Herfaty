43c34dc6992ab47efbbd0b93c3ebf582172362ed
sarahalomar <1saronho@gmail.com>
2022-10-27 15:14:17 +0300

    update search

--- herfaty/lib/CustomerProducts/CustomerProductsList.dart
+++ herfaty/lib/CustomerProducts/CustomerProductsList.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:herfaty/CustomerProducts/CustomerProductDetails.dart';
 import 'package:herfaty/CustomerProducts/productCard.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:herfaty/ShopOwnerOrder/list.dart';
 import 'package:herfaty/models/Product1.dart';
 import 'package:herfaty/constants/color.dart';
 
@@ -20,7 +21,18 @@ class CustomerProductsList extends StatefulWidget {
   State<CustomerProductsList> createState() => _CustomerProductsListState();
 }
 enum Menu { itemOne, itemTwo, itemThree}
+final List<String> productsName=[];
+bool takeName=false;
+ String CatName='';
+
+ Map catCheck={
+ "الخرز والإكسسوار":false,
+"الفخاريات":false,
+"الحياكة والتطريز":false,
+"فنون الورق والتلوين":false,
+ };
 class _CustomerProductsListState extends State<CustomerProductsList> {
+
   //variable to store the category name from categories page
   Stream<List<Product1>> readPrpducts() => FirebaseFirestore.instance
       .collection('Products')
@@ -38,7 +50,9 @@ class _CustomerProductsListState extends State<CustomerProductsList> {
           snapshot.docs.map((doc) => Product1.fromJson(doc.data())).toList());
 
   TextEditingController editingController = TextEditingController();
- 
+ void initState(){
+   CatName=widget.categoryName;
+ }
   
   //======================================================================================
 String searchString = "";
@@ -66,32 +80,32 @@ String typeOfSort="العادي";
           )),
           child: Column(
             children: [
-              Container(
-                 padding: EdgeInsets.symmetric(horizontal: 40),
-                 // margin: EdgeInsets.symmetric(horizontal: 10,vertical: 9),
-          child:  TextField(
+//               Container(
+//                  padding: EdgeInsets.symmetric(horizontal: 40),
+//                  // margin: EdgeInsets.symmetric(horizontal: 10,vertical: 9),
+//           child:  TextField(
             
-                onChanged: (value) {
-          searchString=value;
-        },
+//                 onChanged: (value) {
+//           searchString=value;
+//         },
   
-  decoration: InputDecoration(
-  contentPadding: const EdgeInsets.symmetric(vertical: 1.0, horizontal: 25),    
-      enabledBorder: OutlineInputBorder(
-        borderSide: BorderSide(color: Color.fromARGB(255, 26, 96, 91)),
-      ),
-      focusedBorder: OutlineInputBorder(
-        borderSide:
-            BorderSide(width: 2, color: Color.fromARGB(255, 26, 96, 91)),
-      ),
-      hintText: "البحث",
-      suffixIcon: Icon(Icons.search
-      , color: Color.fromARGB(255, 26, 96, 91),),
-      // border: OutlineInputBorder(
-      //     borderRadius: BorderRadius.all(Radius.circular(29.0)))
-          ),
-),
-              ),
+//   decoration: InputDecoration(
+//   contentPadding: const EdgeInsets.symmetric(vertical: 1.0, horizontal: 25),    
+//       enabledBorder: OutlineInputBorder(
+//         borderSide: BorderSide(color: Color.fromARGB(255, 26, 96, 91)),
+//       ),
+//       focusedBorder: OutlineInputBorder(
+//         borderSide:
+//             BorderSide(width: 2, color: Color.fromARGB(255, 26, 96, 91)),
+//       ),
+//       hintText: "البحث",
+//       suffixIcon: Icon(Icons.search
+//       , color: Color.fromARGB(255, 26, 96, 91),),
+//       // border: OutlineInputBorder(
+//       //     borderRadius: BorderRadius.all(Radius.circular(29.0)))
+//           ),
+// ),
+//               ),
               //تبعد لي البوكس اللي يعرض المنتجات عن الشريط العلوي
               const SizedBox(height: 13),
               Expanded(
@@ -114,13 +128,32 @@ String typeOfSort="العادي";
                           final productItems = snapshot.data!.toList();
                           final data = snapshot.data!;
                           var serchList=[];
-                           if(typeOfSort=='itemThree'){
+                        
+if( catCheck[widget.categoryName]==false){
+ print(productItems.length);  
+   print('ddddddddddddddddddddddddd'); 
+  for (var i = 0; i < productItems.length; i++) {
+    print(productItems[i].name);
+    print('ddddddddddddddddddddddddd'); 
+    if((productsName.contains(productItems[i].name))==false){
+      print('ddddddddddddddddddddddddd');
+      print(productsName.contains(productItems[i].name));
+     
+                              productsName.add(productItems[i].name);
+
+    }
+                                       }   
+
+                        
+               catCheck[widget.categoryName]=true;          
+   takeName=true;
+}
+  if(typeOfSort=='itemThree'){
                                productItems.sort((a, b) {
                           return (b.price)
                               .compareTo((a.price));
                         });
                            }
-
   if(typeOfSort=='itemTwo'){
                                productItems.sort((a, b) {
                           return (a.price)
@@ -276,6 +309,17 @@ String typeOfSort="العادي";
 
         actions: <Widget>[
           // This button presents popup menu items.
+          IconButton(
+        padding: EdgeInsets.only(right: 20),
+        icon: const Icon(
+          Icons.search, //سهم العودة
+          color: Color.fromARGB(255, 26, 96, 91),
+          size: 22.0,
+        ),
+        onPressed: () {
+        showSearch(context: context, delegate: mySearch(),);
+        }, //نخليه يرجع لصفحة المنتجات اللي عند عائشة
+      ),
           PopupMenuButton<Menu>(
                     
             icon: Icon(
@@ -302,7 +346,7 @@ String typeOfSort="العادي";
                     ),
                    const PopupMenuItem<Menu>(
                       value: Menu.itemOne,
-                      child: Text('بلا ترتيب'),
+                      child: Text('الأجدد'),
                     ),
                   ]),
         ],
@@ -336,3 +380,161 @@ String typeOfSort="العادي";
     
   }
   }
+
+  class mySearch extends SearchDelegate{
+
+Stream<List<Product1>> readPrpducts() => FirebaseFirestore.instance
+      .collection('Products')
+      .where("categoryName", isEqualTo: CatName)
+      .where('name',isEqualTo: query)
+      .snapshots()
+      .map((snapshot) =>
+          snapshot.docs.map((doc) => Product1.fromJson(doc.data())).toList());
+
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+      return [
+     IconButton(
+       
+        icon: const Icon(
+          Icons.clear, 
+          color: Color.fromARGB(255, 26, 96, 91),
+          size: 22.0,
+        ),
+        onPressed: () {
+          if(query.isEmpty){
+                      close(context, null);
+          }else{
+          query='';}
+        }, 
+      ),
+    // TODO: implement buildActions
+    
+  ];
+
+    
+  }
+  
+  @override
+  Widget? buildLeading(BuildContext context) {
+     return  IconButton(
+        padding: EdgeInsets.only(right: 20),
+        icon: const Icon(
+          Icons.arrow_back, 
+          color: Color.fromARGB(255, 26, 96, 91),
+          size: 22.0,
+        ),
+        onPressed: () {
+
+          close(context, null);
+        
+        }, 
+      );
+    // TODO: implement buildLeading
+    
+  }
+  
+  @override
+  Widget buildResults(BuildContext context) {
+   
+ return   StreamBuilder<List<Product1>>(
+                      stream: readPrpducts(),
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return const Center(
+                              child: CircularProgressIndicator());
+                        }
+                        if (snapshot.hasError) {
+                          return Text(
+                              'Something went wrong! ${snapshot.error}');
+                        }
+                        if (snapshot.hasData) {
+                          //هنا حالة النجاح في استرجاع البيانات...........................................
+                          final data = snapshot.data!;
+                          if (data.isEmpty) {
+                            return const Center(
+                              child: Text(
+                                'لا توجد لديك منتجات ضمن هذه الفئة',
+                                style: TextStyle(
+                                  fontSize: 18.0,
+                                  fontWeight: FontWeight.w600,
+                                  fontFamily: "Tajawal",
+                                  color: Colors.grey,
+                                ),
+                              ),
+                            );
+                          } else {
+                            final productItems = snapshot.data!.toList();
+                            print(productItems.length);
+                              print("productItems.length");
+                           return ListView.builder(
+                              
+                              itemCount: productItems.length,
+                              itemBuilder: (context, index) => productCard(
+                                itemIndex: index,
+                                product: productItems[index],
+                                press: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) =>
+                                          CustomerProdectDetails(
+                                        // يرسل المعلومات لصفحة المنتج عشان يعرض التفاصيل
+                                        detailsImage: productItems[index].image,
+                                        product: productItems[index],
+                                      ),
+                                    ),
+                                  );
+                                },
+                              ),
+                            );
+                            //..................................................................................
+                          }
+                        } 
+                        else {
+                          return const Center(
+                              child: CircularProgressIndicator());
+                        }
+                      },
+                      
+                    );    
+  }
+  
+  
+  
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    // TODO: implement buildSuggestions
+     List<String> Suggestions=[];
+   
+  for (var i = 0; i < productsName.length; i++) {
+    if(productsName[i].contains(query)){
+      Suggestions.add(productsName[i]);
+    }
+  }
+   
+  
+    return ListView.builder(
+      itemCount:Suggestions.length ,
+      itemBuilder: (context, index) {
+        final sugg=Suggestions[index];
+
+         return ListTile(
+          title: Text(sugg),
+          onTap: () {
+            query=sugg;
+            showResults(context);
+          },
+
+         );
+
+
+      },
+  
+    );
+    
+   
+  
+  }
+    }
