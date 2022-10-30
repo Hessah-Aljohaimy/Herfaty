b9271d686f485d314d1658bc1108be778ec7cec4
hessahju <52304304+Hessah-Aljohaimy@users.noreply.github.com>
2022-10-30 22:16:02 +0300

    commit

---
+++ herfaty/lib/dialogs/Deletedialog.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class DeleteDialog extends StatefulWidget {
+  const DeleteDialog({super.key});
+
+  @override
+  State<DeleteDialog> createState() => _DeleteDialogState();
+}
+
+class _DeleteDialogState extends State<DeleteDialog> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ herfaty/lib/dialogs/Exitdialog.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class ExitDialog extends StatefulWidget {
+  const ExitDialog({super.key});
+
+  @override
+  State<ExitDialog> createState() => _ExitDialogState();
+}
+
+class _ExitDialogState extends State<ExitDialog> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
