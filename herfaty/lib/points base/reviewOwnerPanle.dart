e64f153ca6a2763d36ee9142962eb879e78e76b5
Sarrahx <112611661+Sarrahx@users.noreply.github.com>
2022-11-02 01:47:21 +0300

    shop rating at owner side

--- herfaty/lib/points base/reviewOwnerPanle.dart
+++ herfaty/lib/points base/reviewOwnerPanle.dart
@@ -1,6 +1,11 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:herfaty/rating/ratingsList.dart';
 
 class reviewOwnerPanle extends StatefulWidget {
   const reviewOwnerPanle({super.key});
@@ -10,8 +15,26 @@ class reviewOwnerPanle extends StatefulWidget {
 }
 
 class _reviewOwnerPanleState extends State<reviewOwnerPanle> {
+  double averageShopRating = 0;
+  int numberOfRatings = 0;
+
+  @override
+  void initState() {
+    final FirebaseAuth auth = FirebaseAuth.instance;
+    final User? user = auth.currentUser;
+    String thisShopOwnerId = user!.uid;
+    //--------------------------------------------------------
+    setShopAverageRating(thisShopOwnerId);
+    setNumOfRatings(thisShopOwnerId);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    final FirebaseAuth auth = FirebaseAuth.instance;
+    final User? user = auth.currentUser;
+    String thisShopOwnerId = user!.uid;
+    //--------------------------------------------------------
     return SizedBox(
         height: 60,
         child: Row(
@@ -60,7 +83,18 @@ class _reviewOwnerPanleState extends State<reviewOwnerPanle> {
                               fontWeight: FontWeight.bold,
                               fontFamily: "Tajawal"),
                         ),
-                        onPressed: () {},
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => ratingsList(
+                                thisShopOwnerId: thisShopOwnerId,
+                                averageShopRating: averageShopRating,
+                                numberOfRatings: numberOfRatings,
+                              ),
+                            ),
+                          );
+                        },
                       );
                     }),
                   ),
@@ -81,4 +115,59 @@ class _reviewOwnerPanleState extends State<reviewOwnerPanle> {
           ],
         ));
   }
+
+//||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+  Future<void> setShopAverageRating(String thisOwnerId) async {
+    double existedShopRating = await getShopAverageRating(thisOwnerId);
+    setState(() {
+      averageShopRating = existedShopRating;
+    });
+  }
+
+  //=======================================================================================
+  Future<double> getShopAverageRating(String shopOwnerId) async {
+    double averageShopRating = 0;
+    num sumRating = 0;
+    final shopDoc = await FirebaseFirestore.instance
+        .collection('rating')
+        .where("shopOwnerId", isEqualTo: shopOwnerId)
+        .get();
+    if (shopDoc.size > 0) {
+      for (int i = 0; i < shopDoc.size; i++) {
+        var data = shopDoc.docs.elementAt(i).data() as Map;
+        double thisDocRating = data["starsNumber"];
+        sumRating += thisDocRating;
+      }
+
+      averageShopRating = sumRating / shopDoc.size;
+      String inString = averageShopRating.toStringAsFixed(2);
+      averageShopRating = double.parse(inString);
+    }
+    return averageShopRating;
+  }
+
+//||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+  Future<void> setNumOfRatings(String thisOwnerId) async {
+    int existedRatingsNum = await getNumOfRatings(thisOwnerId);
+    setState(() {
+      numberOfRatings = existedRatingsNum;
+    });
+  }
+
+  //=======================================================================================
+  Future<int> getNumOfRatings(String shopOwnerId) async {
+    int existedRatingsNum = 0;
+    final shopDoc = await FirebaseFirestore.instance
+        .collection('rating')
+        .where("shopOwnerId", isEqualTo: shopOwnerId)
+        .get();
+    if (shopDoc.size > 0) {
+      existedRatingsNum = shopDoc.size;
+      print("shop ratings doc size is ${shopDoc.size}");
+    }
+    return existedRatingsNum;
+  }
+
+//=============================================================================================
+///////////////////////////////////////////////////////////////////////////////
 }
