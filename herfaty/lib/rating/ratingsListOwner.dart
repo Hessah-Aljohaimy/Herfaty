6fac0ca4885115a93e0957f3d64b2994033de9f3
Sarrahx <112611661+Sarrahx@users.noreply.github.com>
2022-11-24 08:43:25 +0300

    refresh ratings list when swiping up

--- herfaty/lib/rating/ratingsListOwner.dart
+++ herfaty/lib/rating/ratingsListOwner.dart
@@ -26,7 +26,6 @@ class _ratingsListOwnerState extends State<ratingsListOwner> {
     final User? user = auth.currentUser;
     String thisOwnerId = user!.uid;
     //====================================================================
-    setShopName(thisOwnerId);
     setShopAverageRating(thisOwnerId);
     setNumOfRatings(thisOwnerId);
     super.initState();
@@ -37,153 +36,153 @@ class _ratingsListOwnerState extends State<ratingsListOwner> {
     final FirebaseAuth auth = FirebaseAuth.instance;
     final User? user = auth.currentUser;
     String thisOwnerId = user!.uid;
+    //streamRatingsHeader(thisOwnerId);
     //====================================================================
     return Scaffold(
       resizeToAvoidBottomInset: false,
       backgroundColor: Colors.white,
       appBar: ratingsListAppBar(context),
-      body: SafeArea(
-        child: Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/images/cartBack1.png'),
-              fit: BoxFit.cover,
+      body: RefreshIndicator(
+        onRefresh: () async {
+          setShopAverageRating(thisOwnerId);
+          setNumOfRatings(thisOwnerId);
+        },
+        child: SafeArea(
+          child: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/cartBack1.png'),
+                fit: BoxFit.cover,
+              ),
             ),
-          ),
-          child: Column(
-            children: [
-              const SizedBox(height: 20),
-              Container(
-                //---------------------------------------------------------------------------
-                //مستطيل في أعلى الليست يعرض نسبة تقييم المتجر وعدد تقييمات
-                padding: const EdgeInsets.all(0),
-                margin: EdgeInsets.only(bottom: 20, left: 19, right: 18),
-                height: 70,
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.amber),
-                  borderRadius: BorderRadius.circular(10),
-                  color: Colors.white,
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.orange.withOpacity(.2),
-                      blurRadius: 5.0,
-                      spreadRadius: .05,
-                    ), //BoxShadow
-                  ],
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    //Star icon
-                    Container(
-                        //padding: const EdgeInsets.only(bottom: 0, right: 10),
-                        height: 50,
-                        width: 50,
-                        child: Image.asset('assets/images/star-100.png')),
-                    // عدد نجوم هذا التقييم رقمًا
-                    Padding(
-                      padding:
-                          const EdgeInsets.only(top: 18.0, left: 10, right: 5),
-                      child: Text(
-                        "${averageShopRating}",
-                        style: const TextStyle(
-                          fontSize: 35.0,
-                          fontWeight: FontWeight.w800,
-                          fontFamily: "Tajawal",
-                          color: kPrimaryColor,
+            child: Column(
+              children: [
+                const SizedBox(height: 20),
+                Container(
+                  //---------------------------------------------------------------------------
+                  //مستطيل في أعلى الليست يعرض نسبة تقييم المتجر وعدد تقييمات
+                  padding: const EdgeInsets.all(0),
+                  margin: EdgeInsets.only(bottom: 20, left: 19, right: 18),
+                  height: 70,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.amber),
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.orange.withOpacity(.2),
+                        blurRadius: 5.0,
+                        spreadRadius: .05,
+                      ), //BoxShadow
+                    ],
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      //Star icon
+                      Container(
+                          //padding: const EdgeInsets.only(bottom: 0, right: 10),
+                          height: 50,
+                          width: 50,
+                          child: Image.asset('assets/images/star-100.png')),
+                      // عدد نجوم هذا التقييم رقمًا
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            top: 18.0, left: 10, right: 5),
+                        child: Text(
+                          "${averageShopRating}",
+                          style: const TextStyle(
+                            fontSize: 35.0,
+                            fontWeight: FontWeight.w800,
+                            fontFamily: "Tajawal",
+                            color: kPrimaryColor,
+                          ),
                         ),
                       ),
-                    ),
-                    //عدد التققيمات
-                    Padding(
-                      padding: const EdgeInsets.only(top: 22.0),
-                      child: Text(
-                        "بناءً على (${numberOfRatings}) من التقييمات",
-                        style: TextStyle(
-                          fontSize: 16.0,
-                          fontWeight: FontWeight.w600,
-                          fontFamily: "Tajawal",
-                          decoration: TextDecoration.underline,
-                          color: Colors.amber,
+                      //عدد التققيمات
+                      Padding(
+                        padding: const EdgeInsets.only(top: 22.0),
+                        child: Text(
+                          "بناءً على (${numberOfRatings}) من التقييمات",
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.w600,
+                            fontFamily: "Tajawal",
+                            decoration: TextDecoration.underline,
+                            color: Colors.amber,
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-              //-------------------------------------------------------------------------------------------
-              Expanded(
-                child: Stack(
-                  children: [
-                    //This is to list all of our items fetched from the DB========================
-                    StreamBuilder<List<ratingModel>>(
-                      stream: readRatings(thisOwnerId),
-                      builder: (context, snapshot) {
-                        if (snapshot.connectionState ==
-                            ConnectionState.waiting) {
-                          return const Center(
-                              child: CircularProgressIndicator());
-                        }
-                        if (snapshot.hasError) {
-                          return Text(
-                              'Something went wrong! ${snapshot.error}');
-                        } else if (snapshot.hasData) {
-                          final ratings = snapshot.data!.toList();
-                          final data = snapshot.data!;
-                          //sort the ratings according to date
-                          ratings.sort((a, b) {
-                            return DateTime.parse(b.dateTime)
-                                .compareTo(DateTime.parse(a.dateTime));
-                          });
-                          if (data.isEmpty) {
+                //-------------------------------------------------------------------------------------------
+                Expanded(
+                  child: Stack(
+                    children: [
+                      //This is to list all of our items fetched from the DB========================
+                      StreamBuilder<List<ratingModel>>(
+                        stream: readRatings(thisOwnerId),
+                        builder: (context, snapshot) {
+                          if (snapshot.connectionState ==
+                              ConnectionState.waiting) {
                             return const Center(
-                              child: Text(
-                                'لا توجد تقييمات  ',
-                                style: TextStyle(
-                                  fontSize: 18.0,
-                                  fontWeight: FontWeight.w600,
-                                  fontFamily: "Tajawal",
-                                  color: Colors.grey,
+                                child: CircularProgressIndicator());
+                          }
+                          if (snapshot.hasError) {
+                            return Text(
+                                'Something went wrong! ${snapshot.error}');
+                          } else if (snapshot.hasData) {
+                            final ratings = snapshot.data!.toList();
+                            final data = snapshot.data!;
+                            //sort the ratings according to date
+                            ratings.sort((a, b) {
+                              return DateTime.parse(b.dateTime)
+                                  .compareTo(DateTime.parse(a.dateTime));
+                            });
+                            if (data.isEmpty) {
+                              return const Center(
+                                child: Text(
+                                  'لا توجد تقييمات  ',
+                                  style: TextStyle(
+                                    fontSize: 18.0,
+                                    fontWeight: FontWeight.w600,
+                                    fontFamily: "Tajawal",
+                                    color: Colors.grey,
+                                  ),
                                 ),
-                              ),
-                            );
+                              );
+                            }
+                            //هنا حالة النجاح في استرجاع البيانات...........................................
+                            else {
+                              return ListView.separated(
+                                separatorBuilder: (_, __) => const Divider(
+                                  height: 0,
+                                  thickness: 1,
+                                  indent: 20,
+                                  endIndent: 20,
+                                  color: kPrimaryColor,
+                                ),
+                                itemCount: ratings.length,
+                                itemBuilder: (context, index) => ratingCard2(
+                                    itemIndex: index,
+                                    ratingItem: ratings[index],
+                                    averageShopRating: averageShopRating),
+                              );
+                            }
+                            //..................................................................................
+                          } else {
+                            return const Center(
+                                child: CircularProgressIndicator());
                           }
-                          //هنا حالة النجاح في استرجاع البيانات...........................................
-                          else {
-                            // return ListView.builder(
-                            //   itemCount: ratings.length,
-                            //   itemBuilder: (context, index) => ratingCard(
-                            //       itemIndex: index,
-                            //       ratingItem: ratings[index],
-                            //       averageShopRating: widget.averageShopRating),
-                            // );
-                            return ListView.separated(
-                              separatorBuilder: (_, __) => const Divider(
-                                height: 0,
-                                thickness: 1,
-                                indent: 20,
-                                endIndent: 20,
-                                color: kPrimaryColor,
-                              ),
-                              itemCount: ratings.length,
-                              itemBuilder: (context, index) => ratingCard2(
-                                  itemIndex: index,
-                                  ratingItem: ratings[index],
-                                  averageShopRating: averageShopRating),
-                            );
-                          }
-                          //..................................................................................
-                        } else {
-                          return const Center(
-                              child: CircularProgressIndicator());
-                        }
-                      },
-                    ),
-                    //==================================================================================
-                  ],
+                        },
+                      ),
+                      //==================================================================================
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
@@ -202,29 +201,6 @@ class _ratingsListOwnerState extends State<ratingsListOwner> {
   }
 
 //===================================================================================
-  Future<String> getShopName(String thisOwneerId) async {
-    String shopName = "";
-    final customersDoc = await FirebaseFirestore.instance
-        .collection('shop_owner')
-        .where("id", isEqualTo: thisOwneerId)
-        .get();
-    if (customersDoc.size > 0) {
-      var data = customersDoc.docs.elementAt(0).data() as Map;
-      shopName = data["shopname"];
-      print("shop name is ${shopName}");
-    }
-    return shopName;
-  }
-
-//======================================================================================
-  Future<void> setShopName(String thisOwnerId) async {
-    String existedName = await getShopName(thisOwnerId);
-    setState(() {
-      thisShopName = existedName;
-    });
-  }
-
-//||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
   Future<void> setShopAverageRating(String thisOwnerId) async {
     double existedShopRating = await getShopAverageRating(thisOwnerId);
     setState(() {
@@ -232,7 +208,7 @@ class _ratingsListOwnerState extends State<ratingsListOwner> {
     });
   }
 
-  //=======================================================================================
+//=======================================================================================
   Future<double> getShopAverageRating(String shopOwnerId) async {
     double averageShopRating = 0;
     num sumRating = 0;
@@ -259,6 +235,36 @@ class _ratingsListOwnerState extends State<ratingsListOwner> {
     int existedRatingsNum = await getNumOfRatings(thisOwnerId);
     setState(() {
       numberOfRatings = existedRatingsNum;
+    });
+  }
+
+  //.................
+  Future<void> streamRatingsHeader(String thisOwnerId) async {
+    CollectionReference reference =
+        FirebaseFirestore.instance.collection('rating');
+    reference.snapshots().listen((querySnapshot) {
+      querySnapshot.docChanges.forEach((change) async {
+        if (change.type == DocumentChangeType.added) {
+          for (var index = 0; index < querySnapshot.size; index++) {
+            var data = querySnapshot.docs.elementAt(index).data() as Map;
+            String ratingOwnerId = data["shopOwnerId"];
+            if (ratingOwnerId == thisOwnerId) {
+              if (mounted) {
+                Future.delayed(Duration(seconds: 60), () async {
+                  double existedShopRating =
+                      await getShopAverageRating(thisOwnerId);
+                  int existedRatingsNum = await getNumOfRatings(thisOwnerId);
+
+                  setState(() {
+                    numberOfRatings = existedRatingsNum;
+                    averageShopRating = existedShopRating;
+                  });
+                });
+              }
+            }
+          }
+        }
+      });
     });
   }
 
